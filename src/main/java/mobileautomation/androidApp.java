a45ce05cce495feacb678580453dd3d7ab0214fe
arvinder <arvinder@172.16.7.172>
2018-11-20 14:18:04 +1100

    Android working website

--- src/main/java/mobileautomation/androidApp.java
+++ src/main/java/mobileautomation/androidApp.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -19,14 +20,13 @@ public class androidApp
 {
     public static void main( String[] args ) throws MalformedURLException
     {
-    	File f=new File("src");
-    	File fs=new File(f,"ApiDemos.apk");
-    	
-        DesiredCapabilities cap=new DesiredCapabilities();
+    	DesiredCapabilities cap=new DesiredCapabilities();
         cap.setCapability(MobileCapabilityType.PLATFORM,"Android");
-        cap.setCapability(MobileCapabilityType.DEVICE_NAME,"ArviNexus5X");
-        cap.setCapability(MobileCapabilityType.APP,fs.getAbsolutePath());
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME,"Arvinder OnePlus");
+        cap.setCapability(MobileCapabilityType.BROWSER_NAME,"Chrome");
        
         AndroidDriver<AndroidElement> driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+        driver.get("https://www.cochlear.com/us/en/home");
+        driver.findElement(By.xpath("//*[text()='Menu']")).click();
     }
 }
